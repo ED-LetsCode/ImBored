5d1c30d695d9b3905ca7c50888293466cdc88dbc
Eray Demirel <113133576+ED-LetsCode@users.noreply.github.com>
2022-09-10 15:24:33 +0200

    Update WhatCanIDoDTO.cs

--- ImBored/WhatCanIDoDTO.cs
+++ ImBored/WhatCanIDoDTO.cs
@@ -8,9 +8,6 @@ namespace ImBored
 {
     internal class WhatCanIDoDTO
     {
-        /// <summary>
-        /// Json Object
-        /// </summary>
         public string activity { get; set; }
         public string type { get; set; }
         public string link { get; set; }
